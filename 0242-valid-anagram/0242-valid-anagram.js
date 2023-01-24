b72eee89e1605ef024c67ff4e79dc8a16ce99c60
Abhishek Patil <106476263+abhi259@users.noreply.github.com>
2023-01-24 19:10:20 +0530

    Time: 105 ms (54.18%), Space: 43.6 MB (67.44%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length ) {
+        return false
+    } else {
+        let hashMapS = {}
+        let hashMapT = {}
+        let length = s.length
+        for (let i=0 ; i<length ; i++) {
+            if (!(s[i] in hashMapS)) {
+                hashMapS[s[i]] = 1
+            } else {
+                hashMapS[s[i]]++
+            }
+            if (!(t[i] in hashMapT)) {
+                hashMapT[t[i]] = 1
+            }  else {
+                hashMapT[t[i]]++
+            }
+        }
+        console.log(hashMapS)
+        console.log(hashMapT)
+
+        for (let ch in hashMapS){
+            if (hashMapS[ch] !== hashMapT[ch]) {
+                return false
+            } 
+        }
+        return true
+    }
+
+};
