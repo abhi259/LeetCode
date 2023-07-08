e75bafbd6d1cad53e0718f3a5bbfa69569854344
Abhishek Patil <106476263+abhi259@users.noreply.github.com>
2023-07-08 16:04:01 +0530

    Time: 83 ms (62.94%), Space: 43.7 MB (68.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,33 +4,72 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length !== t.length ) {
+    // if (s.length !== t.length ) {
+    //     return false
+    // } else {
+    //     let hashMapS = {}
+    //     let hashMapT = {}
+    //     let length = s.length
+    //     for (let i=0 ; i<length ; i++) {
+    //         if (!(s[i] in hashMapS)) {
+    //             hashMapS[s[i]] = 1
+    //         } else {
+    //             hashMapS[s[i]]++
+    //         }
+    //         if (!(t[i] in hashMapT)) {
+    //             hashMapT[t[i]] = 1
+    //         }  else {
+    //             hashMapT[t[i]]++
+    //         }
+    //     }
+    //     console.log(hashMapS)
+    //     console.log(hashMapT)
+
+    //     for (let ch in hashMapS){
+    //         if (hashMapS[ch] !== hashMapT[ch]) {
+    //             return false
+    //         } 
+    //     }
+    //     return true
+    // }
+
+
+    if(s.length !== t.length ) {
         return false
     } else {
+
         let hashMapS = {}
         let hashMapT = {}
-        let length = s.length
-        for (let i=0 ; i<length ; i++) {
-            if (!(s[i] in hashMapS)) {
+
+        for(let i=0; i<s.length; i++) {
+            if(! (s[i] in hashMapS)) {
+               
                 hashMapS[s[i]] = 1
             } else {
                 hashMapS[s[i]]++
             }
-            if (!(t[i] in hashMapT)) {
+             if(! (t[i] in hashMapT)) {
+               
                 hashMapT[t[i]] = 1
-            }  else {
+            } else {
                 hashMapT[t[i]]++
             }
         }
+
         console.log(hashMapS)
         console.log(hashMapT)
 
-        for (let ch in hashMapS){
-            if (hashMapS[ch] !== hashMapT[ch]) {
+        for ( let cha in hashMapS) {
+            if(hashMapS[cha] !== hashMapT[cha]) {
                 return false
-            } 
+             }
         }
-        return true
-    }
+             return true
+       
+    } 
+}
 
-};
+
+
+
+
