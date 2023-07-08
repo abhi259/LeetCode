3e3523f8fe18bafe1dbde2d791c37860a8e06332
Abhishek Patil <106476263+abhi259@users.noreply.github.com>
2023-07-08 16:33:45 +0530

    Time: 117 ms (42.43%), Space: 42.9 MB (41.08%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let answer  = []
+
+    for(let i = 0; i<nums.length; i++ ) {
+        for( let j = i + 1; j<nums.length; j++ ) {
+            if (nums[i] + nums[j] === target ) {
+                answer.push(i)
+                answer.push(j)
+                   return answer
+            }
+
+         
+              
+        }
+    }
+
+};
