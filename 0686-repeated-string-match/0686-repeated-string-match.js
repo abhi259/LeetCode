8f15b780054edcee12fbb4a552c678604bd61f17
Abhishek Patil <106476263+abhi259@users.noreply.github.com>
2023-07-10 16:25:06 +0530

    Time: 49 ms (98%), Space: 44.1 MB (32%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.js
+++ 0686-repeated-string-match/0686-repeated-string-match.js
@@ -5,40 +5,6 @@
  */
 var repeatedStringMatch = function(a, b) {
 
-    const hashMapA = {}
-    const hashMapB = {}
-
-//     for (let i = 0; i<a.length; i++ ) {
-//         if(a[i] in hashMapA )  {
-//             hashMapA[a[i]]++
-//         } else {
-//             hashMapA[a[i]] = 1
-//         }
-//     }
-
-//     for (let i = 0; i<b.length; i++ ) {
-//         if(b[i] in hashMapB )  {
-//             hashMapB[b[i]]++
-//         } else {
-//             hashMapB[b[i]] = 1
-//         }
-//     }
-
-//     console.log(hashMapA)
-//     console.log(hashMapB)
-
-//     let difference =  hashMapB[b[0]] - hashMapA[a[0]] 
-
-//     let output = 0
- 
-
-//     for (key in hashMapA) {
-//         output = output + (hashMapB[key] - hashMapA[key] )
-    
-//     }
-
-//    console.log(output - 1)
-
 
 let string1 = a
 
@@ -61,9 +27,6 @@ let string1 = a
         } else {
             return -1
         }
-
-
-    
 
 };
 
