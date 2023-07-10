f8fc25d5b70ceb78b52130321a854191b739ed3d
Abhishek Patil <106476263+abhi259@users.noreply.github.com>
2023-07-11 00:23:40 +0530

    Time: 116 ms (75.86%), Space: 62.4 MB (69.94%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -4,27 +4,38 @@
  */
 var longestConsecutive = function(nums) {
 
-
     if(nums == null || nums.length === 0) return 0
 
-  const set = new Set(nums)
-  let max = 0
+    const numsSet = new Set(nums)
 
-  for (let num of set ) {
-      if(set.has(num - 1)) continue
+    let max = 0
 
-      let currentNum = num 
-      let currentMax = 1 
+    for (let num of numsSet) {
+       
+        if(numsSet.has(num-1)) continue
 
-      while (set.has(currentNum + 1 )) {
-          currentNum++
-          currentMax++ 
+        let tempMax = 1
+        let tempNum = num
 
-      }
+        while(numsSet.has(tempNum+1)) {
+            tempMax = tempMax + 1
+            tempNum = tempNum + 1 
+        }
 
-      max = Math.max(max, currentMax)
-  }
-
-  return max
-    
+        if(tempMax > max) {
+            max = tempMax
+        }
+    }
+    return max
 };
+
+
+
+
+
+
+
+
+
+
+
