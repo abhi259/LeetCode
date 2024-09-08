c1b6b8b0af5bce27321ccea924ce6b5da9a48f75
Abhishek Patil <106476263+abhi259@users.noreply.github.com>
2024-09-08 19:15:33 +0530

    Time: 67 ms (57.13%), Space: 56.7 MB (90.66%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -4,27 +4,47 @@
  */
 var maxArea = function(height) {
 
-    let lp = 0
-    let rp = height.length - 1
-    let area = 0
+    // let lp = 0
+    // let rp = height.length - 1
+    // let area = 0
 
-    while(lp < rp){
+    // while(lp < rp){
         
-        const tempArea = Math.min(height[lp], height[rp] ) * (rp-lp)
+    //     const tempArea = Math.min(height[lp], height[rp] ) * (rp-lp)
 
-        area = Math.max(area, tempArea);
+    //     area = Math.max(area, tempArea);
 
-        console.log(area)
+    //     console.log(area)
 
-        if( height[lp] < height[rp] ) {
-            lp++
-        } else if ( height[lp] > height[rp] ) {
-            rp--
-        } else {
-            lp++
-            rp--
+    //     if( height[lp] < height[rp] ) {
+    //         lp++
+    //     } else if ( height[lp] > height[rp] ) {
+    //         rp--
+    //     } else {
+    //         lp++
+    //         rp--
+    //     }
+
+    // }
+    // return area 
+
+       let maxWater = 0;
+    let left = 0;
+    let right = height.length - 1;
+
+    while (left < right) {
+        const water = Math.min(height[left], height[right]) * (right - left);
+
+        if (water > maxWater) {
+            maxWater = water;
         }
 
+        if (height[left] > height[right]) {
+            right--;
+        } else {
+            left++;
+        }
     }
-    return area 
+
+    return maxWater;
 };
