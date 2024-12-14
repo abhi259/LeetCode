918497003200e8d6739594620d421b7ec60af317
Abhishek Patil <106476263+abhi259@users.noreply.github.com>
2024-12-14 19:54:52 +0530

    Time: 3 ms (67.54%), Space: 51.5 MB (29.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,19 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let answer  = []
 
-    for(let i = 0; i<nums.length; i++ ) {
-        for( let j = i + 1; j<nums.length; j++ ) {
-            if (nums[i] + nums[j] === target ) {
-                answer.push(i)
-                answer.push(j)
-                   return answer
-            }
+    let hashMap = {}
 
-         
-              
+    for(let i=0 ; i<nums.length; i++) {
+
+        const requiredNumber = target - nums[i] 
+
+        if(hashMap.hasOwnProperty(requiredNumber)) {
+            return [i,hashMap[requiredNumber]]
+        } else {
+            hashMap[nums[i]] = i
         }
     }
-
 };
