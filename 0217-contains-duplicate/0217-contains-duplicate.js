34cbb894b146fd41f56e7bec4ac0f8eb7af69930
Abhishek Patil <106476263+abhi259@users.noreply.github.com>
2025-01-25 15:27:54 +0530

    Time: 19 ms (22.34%), Space: 61.1 MB (63.21%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,16 +4,30 @@
  */
 var containsDuplicate = function(nums) {
 
-const tempSet = new Set()
 
-for( let i=0; i<nums.length ; i++ ) {
-    if(tempSet.has(nums[i])) {
+// const hashMap = {}
+
+// for(let i = 0; i<nums.length; i++ ) {
+
+//     if(hashMap[nums[i]]) {
+//         return true;
+//     } else {
+//         hashMap[nums[i]] = 1 
+//     }
+// }
+// console.log(hashMap)
+// return false 
+// }
+
+const map = {}
+
+for(let i = 0 ; i< nums.length; i++) {
+    if(map.hasOwnProperty(nums[i])) {
         return true
     } else {
-        tempSet.add(nums[i])
+        map[nums[i]] = 1
     }
 }
 
-return false;
-    
-};
+return false 
+}
